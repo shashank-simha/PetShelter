249a56f45ff2ae2c26505d86ba0e23898d9af3c8
shashank-simha <shashank-simha@github.com>
2018-08-26 07:00:41 +0530

    URI matcher added to contentProvider

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -6,6 +6,7 @@ package com.example.android.pets.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -13,29 +14,71 @@ import android.net.Uri;
  * {@link ContentProvider} for Pets app.
  */
 public class PetProvider extends ContentProvider {
-    /** Database helper object */
+    /**
+     * URI matcher code for the content URI for the pets table
+     */
+    private static final int PETS = 100;
+    /**
+     * URI matcher code for the content URI for a single pet in the pets table
+     */
+    private static final int PET_ID = 101;
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+        // The content URI of the form "content://com.example.android.pets/pets" will map to the
+        // integer code {@link #PETS}. This URI is used to provide access to MULTIPLE rows
+        // of the pets table.
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
+        // The content URI of the form "content://com.example.android.pets/pets/#" will map to the
+        // integer code {@link #PET_ID}. This URI is used to provide access to ONE single row
+        // of the pets table.
+        //
+        // In this case, the "#" wildcard is used where "#" can be substituted for an integer.
+        // For example, "content://com.example.android.pets/pets/3" matches, but
+        // "content://com.example.android.pets/pets" (without a number at the end) doesn't match.
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PET_ID);
+    }
+
+    /**
+     * Database helper object
+     */
     private PetDbHelper mDbHelper;
+
     @Override
     public boolean onCreate() {
         mDbHelper = new PetDbHelper(getContext());
         return true;
     }
+
     @Override
     public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
         return null;
     }
+
     @Override
     public String getType(Uri uri) {
         return null;
     }
+
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
         return null;
     }
+
     @Override
     public int delete(Uri uri, String s, String[] strings) {
         return 0;
     }
+
     @Override
     public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
         return 0;
