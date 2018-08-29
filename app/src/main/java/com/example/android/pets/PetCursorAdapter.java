f407196b85c0ffe6ef16c4fd06eb391d6619c16b
shashank-simha <shashank-simha@github.com>
2018-08-29 07:37:40 +0530

    Display “Unknown breed” for pets without breed

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -6,6 +6,7 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -57,12 +58,21 @@ public class PetCursorAdapter extends CursorAdapter {
         // Find individual views that we want to modify in the list item layout
         TextView nameTextView = (TextView) view.findViewById(R.id.name);
         TextView summaryTextView = (TextView) view.findViewById(R.id.summary);
+
         // Find the columns of pet attributes that we're interested in
         int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
         int breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
+
         // Read the pet attributes from the Cursor for the current pet
         String petName = cursor.getString(nameColumnIndex);
         String petBreed = cursor.getString(breedColumnIndex);
+
+        // If the pet breed is empty string or null, then use some default text
+        // that says "Unknown breed", so the TextView isn't blank.
+        if (TextUtils.isEmpty(petBreed)) {
+            petBreed = context.getString(R.string.unknown_breed);
+        }
+
         // Update the TextViews with the attributes for the current pet
         nameTextView.setText(petName);
         summaryTextView.setText(petBreed);
